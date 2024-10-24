01b13db3ed3d672ad0652d045f07d7e3fded6467
“ievatm” <Ieva.atmine@gmail.com>
2024-10-24 16:20:18 +0200

    loop to create grid added loop in the function to create flex grid

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ const container = document.querySelector(".flex-container");
 
 function createGrid(size) {
     container.innerHTML = " ";
+
+    for (let i = 0; i <size * size; i++){
+        const div = document.createElement("div");
+        div.classList.add ("flex-item");
+        container.appendChild(div);
+    }
 }
+
+
